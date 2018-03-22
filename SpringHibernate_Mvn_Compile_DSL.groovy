8b264eb923a77d50291afc713ef58af6020e5d67
tejas1203 <35693295+tejas1203@users.noreply.github.com>
2018-03-22 17:41:17 +0530

    Update SpringHibernate_Mvn_Compile_DSL.groovy

--- SpringHibernate_Mvn_Compile_DSL.groovy
+++ SpringHibernate_Mvn_Compile_DSL.groovy
@@ -1,10 +1,13 @@
 job('Spring3Hibernate Compile') {
     scm {
-        git('https://github.com/opstr/ContinuousIntegration.git',master')
+        git {
+            url("https://github.com/opstr/ContinuousIntegration.git")
+            createTag(false)
     }
    
     steps {
         maven{
+         mavenInstallation("Maven 3.5.3")
          rootPOM('Spring3HibernateApp/pom.xml')
          goals('clean compile')   
         }
