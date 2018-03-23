50b08d0147937eb406ab03d1e2172e3c48be4157
Rajat131193 <35720457+Rajat131193@users.noreply.github.com>
2018-03-23 15:18:53 +0530

    Update SpringHibernate_Mvn_CodeCoverage_DSL.groovy

--- SpringHibernate_Mvn_CodeCoverage_DSL.groovy
+++ SpringHibernate_Mvn_CodeCoverage_DSL.groovy
@@ -16,6 +16,11 @@ job('Spring3Hibernate Code Coverage') {
          rootPOM('Spring3HibernateApp/pom.xml')
          goals('cobertura:cobertura')     
         }
+    post {
+        always {
+            step([$class: 'CoberturaPublisher', autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/target/coverage.xml', failUnhealthy: false, failUnstable: false, maxNumberOfBuilds: 0, onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false])
+        }
+    }
       
     }
 }
