fe3a576b63b68f14aa25104e44c2783a46db8f05
tejas1203 <35693295+tejas1203@users.noreply.github.com>
2018-03-23 14:49:38 +0530

    Update and rename Spring3_HibernateCodeQuality_DSL to SpringHibernate_Mvn_CodeQuality_DSL.groovy

---
+++ SpringHibernate_Mvn_CodeQuality_DSL.groovy
@@ -0,0 +1,21 @@
+
+job('Spring3Hibernate Code Quality') {
+    scm {
+        git {
+            remote
+            {
+            url('https://github.com/opstr/ContinuousIntegration.git')
+            }
+            branch('master')  
+        }
+    }
+   
+    steps {
+        maven{
+         mavenInstallation('Maven')
+         rootPOM('Spring3HibernateApp/pom.xml')
+         goals('')     
+        }
+      
+    }
+}
