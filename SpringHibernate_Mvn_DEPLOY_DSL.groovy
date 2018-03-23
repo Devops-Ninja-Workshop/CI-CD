910f74081bdc6ca73327918cdf344f46577e891d
Rajat131193 <35720457+Rajat131193@users.noreply.github.com>
2018-03-23 16:40:24 +0530

    Update SpringHibernate_Mvn_DEPLOY_DSL.groovy

--- SpringHibernate_Mvn_DEPLOY_DSL.groovy
+++ SpringHibernate_Mvn_DEPLOY_DSL.groovy
@@ -1,1 +1,21 @@
 
+job('Spring3Hibernate Code Stability') {
+    scm {
+        git {
+            remote
+            {
+            url('https://github.com/opstr/ContinuousIntegration.git')
+            }
+            branch('master')
+        }
+    }
+   
+    steps {
+        maven{
+         mavenInstallation('Maven')
+         rootPOM('Spring3HibernateApp/pom.xml')
+         goals('jar:jar')     
+        }
+      
+    }
+}
