61c7e2cbf8524b64bc48b0f1e53ef822f9eb3215
tejas1203 <35693295+tejas1203@users.noreply.github.com>
2018-03-23 14:39:51 +0530

    Update SpringHibernate_Mvn_Compile_DSL.groovy

--- SpringHibernate_Mvn_Compile_DSL.groovy
+++ SpringHibernate_Mvn_Compile_DSL.groovy
@@ -12,9 +12,6 @@ job('Spring3Hibernate Compile') {
     steps {
         maven{
          mavenInstallation('Maven')
-            goals("build-helper:parse-version")
-						goals("versions:set")
-						property("newVersion", "\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-\${BUILD_NUMBER}")
          rootPOM('Spring3HibernateApp/pom.xml')
          goals('clean compile')     
         }
