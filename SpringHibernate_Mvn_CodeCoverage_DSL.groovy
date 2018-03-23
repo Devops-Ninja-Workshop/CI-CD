2e7b7903e6158cc2aeb3f07ecc2ba34c5a3a7ce8
Rajat131193 <35720457+Rajat131193@users.noreply.github.com>
2018-03-23 14:45:00 +0530

    Update SpringHibernate_Mvn_CodeCoverage_DSL.groovy

--- SpringHibernate_Mvn_CodeCoverage_DSL.groovy
+++ SpringHibernate_Mvn_CodeCoverage_DSL.groovy
@@ -1,1 +1,21 @@
 
+job('Spring3Hibernate Code Coverage') {
+    scm {
+        git {
+            remote
+            {
+            url('https://github.com/opstr/ContinuousIntegration.git')
+            }
+            branch('master')  
+        }
+    }
+   
+    steps {
+        maven{
+         mavenInstallation('Maven')
+         rootPOM('Spring3HibernateApp/pom.xml')
+         goals('cobertura:cobertura')     
+        }
+      
+    }
+}
